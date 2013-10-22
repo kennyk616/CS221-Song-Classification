83f6ee3b6704e76dbe6a02fc064c983a6219144c
Kenny Kao <kennyk616@gmail.com>
2013-10-22 01:19:43 -0700

    updated base_classifier. Still 1 bug

--- base_classifier.py
+++ base_classifier.py
@@ -4,20 +4,31 @@ sys.path.append(lib_path)
 import hdf5_getters
 import numpy as np
 import matplotlib.pyplot as plt
-
+import json
+import pdb
 
 def get_all_timbre(track_paths):
 	dic_of_timbres = dict()
+	count = 0
 	for track_id in track_paths:
-		h5 = hdf5_getters.open_h5_file_read(track_paths[track_id])
-		timbre_by_segment = hdf5_getters.get_segments_timbre(h5)
+		path = "./MSD-SHS/train/"+track_paths[track_id]
+		h5 = hdf5_getters.open_h5_file_read(path)
+		try:
+			timbre_by_segment = hdf5_getters.get_segments_timbre(h5)
+		except Exception as e:
+			print repr(e)
+			pdb.post_mortem()
 		h5.close()
 		avg_timbre = np.mean(timbre_by_segment, axis=0)
-		list_of_timbers.add(track_id, avg_timbre)
+		dic_of_timbres[track_id] = avg_timbre
+		count +=1
+		if count > 500:
+			break
 	return dic_of_timbres
 
 def compute_distance(dic_timbres, track_id1, track_id2):
 	v1 = dic_timbres[track_id1]
+	#print track_id2 in dic_timbres
 	v2 = dic_timbres[track_id2]
 	return np.linalg.norm(v1-v2)
 
@@ -25,15 +36,15 @@ def compute_distance(dic_timbres, track_id1, track_id2):
 def is_same_clique(tracks_info, track_id1, track_id2):
 	track1 = tracks_info[track_id1]
 	track2 = tracks_info[track_id2]
-	return track1[clique_name]==track2[clique_name]
+	return track1["clique_name"]==track2["clique_name"]
 
 
 def compute_all_distance(track_paths, tracks_info):
 	dic_timbres = get_all_timbre(track_paths)
 	dist_same_clique = []
 	dist_diff_clique = []
-	for track_id1 in track_paths:
-		for track_id2 in track_paths:
+	for track_id1 in dic_timbres:
+		for track_id2 in dic_timbres:
 			if track_id1 == track_id2:
 				continue
 			if(is_same_clique(tracks_info, track_id1, track_id2)):
@@ -48,17 +59,28 @@ def compute_all_distance(track_paths, tracks_info):
 def plot_histogram(data):
 	min_val = min(min(l) for l in data)
 	max_val = max(max(l) for l in data)
-	bins = numpy.linspapce(min_val, max_val, 100)
+	bins = np.linspace(min_val, max_val, 100)
 
-	for l in data:
-		plt.hist(l, bins, alpha=0.5)
+	print "min_val", min_val
+	print "max_val", max_val
+	print "bins", bins
+
+	plt.hist(data[0], bins, alpha=0.5)
+	plt.hist(data[1], bins, alpha=0.5)
+
+	#for l in data:
+	#	plt.hist(l, bins, alpha=0.5)
 	plt.show()
 
 
 def main():
-	track_paths = dict() #place holder for the dictionary of paths
-	tracks_info = dict() #place holder for track info
-	dist_same_clique, dist_diff_clique = compute_all_distance(track_paths, tracks_info)
+	json_data_paths=open('./MSD-SHS/shs_dataset_train/shs_dataset_train.trackpaths.json')
+	json_data=open('./MSD-SHS/shs_dataset_train/shs_dataset_train.tracks.json')
+	track_paths = json.load(json_data_paths) #place holder for the dictionary of paths
+	track_info = json.load(json_data) #place holder for track info
+	#print track_paths["TRQPZPQ128F4292D21"]
+	#print track_info["TRQPZPQ128F4292D21"]
+	dist_same_clique, dist_diff_clique = compute_all_distance(track_paths, track_info)
 	plot_histogram([dist_same_clique, dist_diff_clique])
 
 
