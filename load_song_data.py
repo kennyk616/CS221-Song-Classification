0b0eb06cccca5d3dfa510402ab53834b67e81fe7
Ian Tenney <iftenney@gmail.com>
2013-11-02 21:54:56 -0700

    Bug fixes and additional helper functions in load_song_data and logistic modules.

--- load_song_data.py
+++ load_song_data.py
@@ -17,35 +17,46 @@ DATAPATH = "./MSD-SHS/train/"
 # are TID, AID, work, and clique_name
 
 class Track:
-	def __init__(self, track_id, path, clique):
-		self.id = track_id
-		self.path = path
-		self.clique = clique
-	
-	def get_segments_timbre():
-		path = DATAPATH + self.path
-		h5 = hdf5_getters.open_h5_file_read(path)
-		try:
-			timbre_by_segment = hdf5_getters.get_segments_timbre(h5)
-		except Exception as e:
-			print repr(e)
-			timbre_by_segment = None
-		return timbre_by_segment
+    def __init__(self, track_id, path, clique):
+        self.id = track_id
+        self.path = path
+        self.clique = clique
+    
+    def get_segments_timbre(self):
+        path = DATAPATH + self.path
+        h5 = hdf5_getters.open_h5_file_read(path)
+        try:
+            timbre_by_segment = hdf5_getters.get_segments_timbre(h5)
+        except Exception as e:
+            print repr(e)
+            timbre_by_segment = None
+        return timbre_by_segment
 
 
 
 class Track_dataset:
-	def __init__(self, data_set):
-		def get_dic(path):
-			json_data = open(path)
-			return json.load(json_data)
+    def __init__(self, data_set):
+        def get_dic(path):
+            json_data = open(path)
+            return json.load(json_data)
 
-		self.track_paths = get_dic('./MSD-SHS/shs_dataset_' + data_set + '/shs_dataset_' + data_set + '.trackpaths.json')
-		self.track_info = get_dic('./MSD-SHS/shs_dataset_' + data_set + '/shs_dataset_' + data_set + '.tracks.json')
-		self.track_cliques_shs = get_dic('./MSD-SHS/shs_dataset_' + data_set + '/shs_dataset_' + data_set + '.cliques.json')
+        self.track_paths = get_dic('./MSD-SHS/shs_dataset_' + data_set + '/shs_dataset_' + data_set + '.trackpaths.json')
+        self.track_info = get_dic('./MSD-SHS/shs_dataset_' + data_set + '/shs_dataset_' + data_set + '.tracks.json')
+        self.track_cliques_shs = get_dic('./MSD-SHS/shs_dataset_' + data_set + '/shs_dataset_' + data_set + '.cliques.json')
 
-	def get_track(self, track_id):
-		return Track(track_id, self.track_paths[track_id], self.track_info[track_id]['clique_name'])
+    def prune(self, ntracks=1000):
+        """Prune the dataset down to a smaller number of tracks."""
+        kf = lambda (k,v): v['clique_name']
+        def pare_dict(d,n):
+            return dict(sorted(d.items(), key=kf)[:n])
+        self.track_info = pare_dict(self.track_info, ntracks)
+        self.track_paths = {k:v for k,v in self.track_paths.items() if k in self.track_info}
 
+
+    def get_track(self, track_id):
+        return Track(track_id, self.track_paths[track_id], self.track_info[track_id]['clique_name'])
+
+    def get_tracks(self):
+        return [self.get_track(track_id) for track_id in self.track_paths.keys()]
 
 
