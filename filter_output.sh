d5fad5f8c41f8608e9e9a315a3adbefe7827a10d
Ian Tenney <iftenney@gmail.com>
2013-12-11 16:14:37 -0800

    Updated data extractor output to be more spreadsheet-friendly.

--- filter_output.sh
+++ filter_output.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# filter output files
+# filter output files, extract values, and print in a spreadsheet-friendly format
 # usage: filter_output.sh <line_glob> <fname_glob>
 
 # grep -h "KNN relax test" *k_7*scale* | grep -Po '[0-9.]+' | sed ':a;N;$!ba;s/\n/\t/g'
-echo "# Field: <$1> :: match: *$2*"
-grep -h "$1" *$2* | grep -Po '[0-9.]+' | sed ':a;N;$!ba;s/\n/\t/g'
+echo -n "# Field: <$1> :: match: *$2*,"
+grep -h "$1" *$2* | grep -Po '[0-9.]+' | sed ':a;N;$!ba;s/\n/,/g'
